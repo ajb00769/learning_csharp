fc36e0e06e40dd75181d8a4e62a32e5d9ca27324
Allen Bercero <abercero@pop-os.localdomain>
2024-05-21 06:29:50 +0800

    warning suppression exercise finished

--- Directives/DirectiveWarningSuppression/Program.cs
+++ Directives/DirectiveWarningSuppression/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WarningSuppression
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+#pragma warning disable CS0251
+            System.Console.WriteLine(nums[-1]);
+#pragma warning restore CS0251
+            /*
+                Although the warning will be suppressed allowing the code to run 
+                it will still return an unhandled exception for the suppressed
+                error.
+
+                I guess a real-word use case for this is custom Exception handling
+                so that the developer can create custom messages for the suppressed
+                error so that troubleshooting can be easier for support engineers.
+
+                Though warning suppression should be done with caution because it runs
+                the risk of obscuring the root cause of the issue which is heavily
+                detailed in the built in warnings with proper tracebacks.
+            */
+        }
+    }
+}
