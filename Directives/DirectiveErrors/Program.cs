9ab0130cbdbea32556d4692eaeebb9bdcbd231e6
Allen Bercero <abercero@pop-os.localdomain>
2024-05-25 08:41:24 +0800

    updated DirectiveErrors

--- Directives/DirectiveErrors/Program.cs
+++ Directives/DirectiveErrors/Program.cs
@@ -7,6 +7,7 @@ namespace Errors
         static void Main()
         {
 #warning Code is deprecated.
+#line 10 "Error happened here. Told you this code was deprecated"
 #error Deprecated code.
 #error version
             System.Console.WriteLine("Hello, world!");
@@ -21,4 +22,8 @@ error in the pre-compile phase preventing the code from compiling.
 Some use cases involve the code being deprecated. The #error version
 directive is treated specially where it outputs the compiler version
 and language (C#) version under the CS8304 error. 
+
+The #line directive is only for debugging so you can manually point
+to a certain line of code that caused the error because sometimes
+traceback messages can be long or inaccurate.
 */
--- Directives/DirectiveErrors/obj/Debug/net8.0/DirectiveErrors.AssemblyInfo.cs
+++ Directives/DirectiveErrors/obj/Debug/net8.0/DirectiveErrors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DirectiveErrors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1cf3a5781dc2b2a0076ae8f94f571f4cb8a5a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3dd840a86b28acbf0f8918c8274917caa11e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("DirectiveErrors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DirectiveErrors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
